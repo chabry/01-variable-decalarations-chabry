4e702471ad343bcbbecd2f8e9d6d6d996eefa027
Chabry <chabry01@gmail.com>
2024-11-21 15:08:41 +0100

    EXERCICE 1 FINI

--- variables.js
+++ variables.js
@@ -10,26 +10,36 @@ function declareVarOne() {
 // Function 2
 function declareVarTwo() {
   // Your code here
+  const userName = 'John Doe'
+  return userName
 }
 
 // Function 3
 function declareVarThree() {
   // Your code here
+  const isCompleted = true
+  return isCompleted
 }
 
 // Function 4
 function declareVarFour() {
   // Your code here
+  const emptyArray = []
+  return emptyArray
 }
 
 // Function 5
 function declareVarFive() {
   // Your code here
+  const nullVar = null
+  return nullVar
 }
 
 // Function 6
 function declareVarSix() {
   // Your code here
+  let undefinedVar
+  return undefinedVar
 }
 
 module.exports = {
